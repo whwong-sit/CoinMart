1c9a5a0d33d9f5bbed88be3b2c9d85a0eb98e87b
leostn <tsun2233@uni.sydney.edu.au>
2017-06-03 20:58:49 +1000

    update database

--- coinmart/test_schema.sql
+++ coinmart/test_schema.sql
@@ -4,13 +4,25 @@ create table users (
   password text not null,
   email text not null
 );
+
+insert into users values('admin', 'default', 'admin@uni.sydney.edu.au');
 drop table if exists user_watchlists;
 create table user_watchlists(
   username text,
   watchlist_id text,
   foreign key(username) references users(username),
-  primary key(username, watchlist_id)
+   PRIMARY KEY (username,watchlist_id)
 );
+
+drop table if exists userwatchlists;
+create table userwatchlists(
+  username text,
+  watchlist_id text,
+  currency2 text,
+  foreign key(username) references users(username),
+  PRIMARY KEY (username,watchlist_id,currency2)
+);
+
 drop table if exists watchlist_items;
 create table watchlist_items(
   watchlist_id text,
@@ -18,13 +30,11 @@ create table watchlist_items(
   currency text,
   value real not null,
   foreign key(watchlist_id) references user_watchlists(watchlist_id),
-  primary key(watchlist_id,cryptocurrency,currency)
+  PRIMARY KEY (watchlist_id,cryptocurrency,currency)
 );
-
-insert into users values('admin', 'default', 'admin@uni.sydney.edu.au');
-insert into users values('Test', 'Test_123', 'test@uni.sydney.edu.au');
-insert into user_watchlists values('Test', 'bitcoin');
-insert into user_watchlists values('Test', 'ripple');
 insert into watchlist_items values('bitcoin', 'Bitcoin', 'EUR', 1754.22019599);
 insert into watchlist_items values('ripple', 'Ripple', 'EUR', 0.3028863965);
-select * from user_watchlists, watchlist_items where user_watchlists.watchlist_id = watchlist_items.watchlist_id and user_watchlists.username = 'Test'
+
+
+
+
