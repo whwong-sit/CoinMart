c0a281a822a89cdcf13fdf5b43ac981b12e6cf09
leostn <tsun2233@uni.sydney.edu.au>
2017-06-15 01:03:41 +1000

    merg grop B's data base

--- coinmart/schema.sql
+++ coinmart/schema.sql
@@ -10,37 +10,36 @@ insert into users values('admin', 'default', 'admin@uni.sydney.edu.au');
 
 drop table if exists user_watchlists;
 create table user_watchlists(
+  watchlist_id integer primary key AUTOINCREMENT,
   username text,
   watchlist_name text,
-  foreign key(username) references users(username),
-  PRIMARY KEY (username,watchlist_name)
+  foreign key(username) references users(username)
 );
 
 drop table if exists watchlist_items;
 create table watchlist_items(
-  username text,
-  watchlist_name text,
+ watchlist_id integer,
   cryptocurrency text,
   currency text,
   current_value real not null,
   current_time timestamp not null,
-  foreign key(watchlist_name,username) references user_watchlists(watchlist_name,username),
-  primary key(username,watchlist_name,cryptocurrency,currency)
+  foreign key(watchlist_id) references user_watchlists(watchlist_id),
+  primary key(watchlist_id,cryptocurrency,currency)
 );
 
 drop table if exists historical_watchlist_data;
 create table historical_watchlist_data(
-
+  watchlist_id integer,
   username text,
   watchlist_name text,
   cryptocurrency text,
   currency text,
   old_value real not null,
   old_time timestamp not null,
-  foreign key(watchlist_name,username) references user_watchlists(watchlist_name,username),
-  primary key(username,watchlist_name,cryptocurrency,currency)
+  foreign key(watchlist_id) references user_watchlists(watchlist_id),
+ primary key(watchlist_id,cryptocurrency,currency)
 );
-
+insert into user_watchlists values(1, 'admin', 'bitcoinEUR');
 
 
 
