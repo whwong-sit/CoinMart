0727bf8d137a5161b5a771458a16e0f1ee7f1b85
Danny94W <daniel.b.wallis@outlook.com>
2017-06-24 17:40:41 +1000

    a

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,3 @@
-import os
 import threading
 from selenium import webdriver
 import coinmart
@@ -9,11 +8,10 @@ def before_all(context):
     context.server = coinmart.coinmart
     context.thread = threading.Thread(target=context.server.test_server)
     context.thread.start()  # start flask app server
-    chromedriver = "C:/Users/Daniel/Documents/GitHub/CoinMart/features/chromedriver.exe"
-    os.environ["webdriver.chrome.driver"] = chromedriver
-    context.browser = webdriver.Chrome(chromedriver)
+    context.browser = webdriver.Chrome()
     context.server_address = "http://" + app.config['SERVER_NAME']
     context.home = context.server_address
+
 
 
 def after_all(context):
